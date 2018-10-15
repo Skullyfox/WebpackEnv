fcd1a04009b634029d97cb02016c562952f49a64
Logan Wilquin <Skullyfox.yt@gmail.com>
2018-10-15 09:36:33 +0200

    JavaScript Loader with Babel added

--- config/Webpack/Webpack.prod.js
+++ config/Webpack/Webpack.prod.js
@@ -46,7 +46,17 @@ module.exports = merge(common, {
         "css-loader",
         "sass-loader"
         ]
-      },
+    },{
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }
+
     ]
   },
 });
